0ec3fbd6cd095adb1430dc65e38f149b476825d3
Sagar Mitra <mitrasagar10@gmail.com>
2025-07-08 00:03:31 +0530

    sidebar task Link update

--- src/components/Sidebar/SidebarTasks.jsx
+++ src/components/Sidebar/SidebarTasks.jsx
@@ -28,27 +28,27 @@ const SidebarTasks = () => {
   return (
     <div className="p-3 h-48 w-full mt-2 flex flex-col justify-evenly ">
       <p className="text-xs font-semibold tracking-wider">TASKS</p>
-      <div className="group/tasks flex py-2 px-5 justify-between items-center cursor-pointer rounded hover:bg-gray-200  duration-200">
-        <Link className="flex gap-1.5 items-center" to="/">
+      <Link to="/" className="group/tasks flex py-2 px-5 justify-between items-center cursor-pointer rounded hover:bg-gray-200  duration-200">
+        <div className="flex gap-1.5 items-center" >
           <LuLayoutDashboard className="text-lg text-blue-500" />
           <h3 className="text-base font-medium opacity-60 group-hover/tasks:opacity-100">Dashboard</h3>
-        </Link>
+        </div>
         <p className=" opacity-60">{taskForToday.length + upComingTask.length}</p>
-      </div>
-      <div className="group/tasks flex py-2 px-5 rounded justify-between items-center cursor-pointer hover:bg-gray-200  duration-200">
-        <Link className="flex gap-1.5 items-center" to="/today">
+      </Link>
+      <Link to="/today" className="group/tasks flex py-2 px-5 rounded justify-between items-center cursor-pointer hover:bg-gray-200  duration-200">
+        <div className="flex gap-1.5 items-center">
           <BsCalendarDate className="text-lg text-green-600"/>
           <h3 className="text-base font-medium opacity-60 group-hover/tasks:opacity-100">Today</h3>
-        </Link>
+        </div>
         <p className=" opacity-60">{taskForToday.length}</p>
-      </div>
-      <div className="group/tasks  flex py-2 px-5 rounded justify-between items-center cursor-pointer hover:bg-gray-200  duration-200">
-        <Link className="flex gap-1.5 items-center" to="/upcoming">
+      </Link>
+      <Link to="/upcoming" className="group/tasks  flex py-2 px-5 rounded justify-between items-center cursor-pointer hover:bg-gray-200  duration-200">
+        <div className="flex gap-1.5 items-center">
           <MdOutlineCalendarMonth className="text-lg text-purple-600"/>
           <h3 className="text-base font-medium opacity-60 group-hover/tasks:opacity-100">Upcoming</h3>
-        </Link>
+        </div>
         <p className=" opacity-60 ">{upComingTask.length}</p>
-      </div>
+      </Link>
     </div>
   );
 };
