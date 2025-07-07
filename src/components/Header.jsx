b49dc08718a66ec9574dc622e69f6a7cc112ba38
Sagar Mitra <mitrasagar10@gmail.com>
2025-07-07 23:54:29 +0530

    feature: header profile picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,26 +8,28 @@ const Header = () => {
   const { loginInfo } = useContext(AuthContext);
   console.log(loginInfo);
 
- 
-
   return (
-    <div className="mb-2 p-4 flex justify-between items-center">
+    <div className="mb-1 p-3 flex justify-between items-center">
       <h1 className="font-garaigit text-4xl font-bold ">tasky</h1>
-      <div className="w-36 p-1 border">
+      <div>
         {Object.keys(loginInfo).length !== 0 ? (
           <div className="w-full flex items-center justify-between">
-            <h4 className="text-sm">{loginInfo.userName}</h4>
-            {/* {loginInfo.profilePhoto && <ProfilePicture file={loginInfo.profilePhoto} />} */}
+            {/* <h4 className="text-lg ">{loginInfo.userName}</h4> */}
+            <img
+              className="w-14 h-14 rounded-full mx-auto p-1 border-2 border-gray-500"
+              src={loginInfo.profilePhoto}
+              alt=""
+            />
           </div>
         ) : (
           <Link
-            className="group/login flex gap-1 items-center justify-center py-1 px-3 rounded-lg hover:bg-[#f4f4f4]"
+            className="group/login flex border border-black gap-1 items-center justify-center py-1 px-3 rounded-lg  hover:bg-blue-400 hover:border-blue-400 hover:text-white hover:font-medium duration-300"
             to="/login"
           >
-            <h3 className="font-ltsuperiorMono opacity-70 text-lg font-semibold group-hover/login:opacity-100">
+            <MdLogin className="text-lg opacity-70 group-hover/login:opacity-100 duration-200" />
+            <h3 className="opacity-70 text-lg font-semibold group-hover/login:opacity-100 duration-200">
               Login
             </h3>
-            <MdLogin className="text-lg opacity-70 group-hover/login:opacity-100" />
           </Link>
         )}
       </div>
